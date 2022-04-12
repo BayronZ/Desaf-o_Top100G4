9e9ecfdaad4958458a2024daf4693bc710fd62ce
BayronZ <bayron.zavalag@gmail.com>
2022-04-12 19:41:55 -0400

    Finished: punto 6

--- cineteca.sql
+++ cineteca.sql
@@ -1,8 +1,9 @@
+-- 1. Crear una base de datos llamada películas. (1 Punto)
 \c mrcoco
 DROP DATABASE peliculas;
 CREATE DATABASE peliculas;
-\c peliculas;
-
+\c peliculas
+-- 2. Cargar ambos archivos a su tabla correspondiente. (1 Punto)
 CREATE TABLE peliculas(
     id SERIAL,
     Pelicula VARCHAR(100),
@@ -11,22 +12,44 @@ CREATE TABLE peliculas(
     PRIMARY KEY(id)
 );
 
-SELECT * FROM peliculas;
-
 \copy peliculas FROM 'peliculas.csv' csv header;
 
 SELECT * FROM peliculas LIMIT 10;
 
 CREATE TABLE reparto(
-    id INT,
-    Reparto VARCHAR(100),
-    FOREIGN KEY (id) REFERENCES peliculas(id)
+    pelicula_id INT,
+    actores VARCHAR(100),
+    FOREIGN KEY (pelicula_id) REFERENCES peliculas(id)
 );
 
 \copy reparto FROM 'reparto.csv' csv;
 
 SELECT * FROM reparto LIMIT 10;
 
-SELECT id AS ID FROM peliculas WHERE Pelicula = 'Titanic';
+-- 3. Obtener el ID de la película “Titanic”. (1 Punto)
+SELECT id FROM peliculas WHERE Pelicula = 'Titanic';
 
-SELECT reparto from reparto WHERE id = 1;
+-- 4. Listar a todos los actores que aparecen en la película "Titanic". (1 Puntos)
+SELECT * FROM reparto WHERE pelicula_id = 2;
+-- 5. Consultar en cuántas películas del top 100 participa Harrison Ford. (2 Puntos)
+SELECT COUNT(*) FROM reparto WHERE actores = 'Harrison Ford';
+-- 6. Indicar las películas estrenadas entre los años 1990 y 1999 ordenadas por título de
+-- manera ascendente. (1 punto)
+SELECT Pelicula FROM peliculas WHERE Año_Estreno BETWEEN 1990 AND 1999 ORDER BY Pelicula ASC;
+-- 7. Hacer una consulta SQL que muestre los títulos con su longitud, la longitud debe ser
+-- nombrado para la consulta como “longitud_titulo”. (1 punto)
+-- 8. Consultar cual es la longitud más grande entre todos los títulos de las películas.
+-- (2 punto)
+
+
+
+
+
+
+
+
+
+
+
+
+
